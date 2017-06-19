cf80f203d7a49b4cbf44859ef932f280bd7305ab
Mike Hook <hook.mike@gmail.com>
2017-06-19 14:41:23 +0100

    Made VideoSource an umbraco property.

---
+++ website/masterpages/MstcVideoContent.master.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using umbraco.NodeFactory;
+
+public partial class masterpages_MstcVideoContent : System.Web.UI.MasterPage
+{
+    public string VideoSource { get; set; }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack == false && Node.GetCurrent() != null && Node.GetCurrent().GetProperty("videoSource") != null)
+        {
+            VideoSource = Node.GetCurrent().GetProperty("videoSource").Value;
+        }
+    }
+}
