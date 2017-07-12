5b4e962ac964bc0ae2bb3ae8f5f34139847bef31
Mike Hook <hook.mike@gmail.com>
2017-07-12 13:43:59 +0100

    Fixed path to Web.config in beforePackaging script.

--- deploymentScripts/beforePackaging.ps1
+++ deploymentScripts/beforePackaging.ps1
@@ -1,11 +1,11 @@
 Write-Host 'Hey, this is my before packaging script'
 
 #$root = 'D:\dev\MSTC\website'
-$root =  $env:APPVEYOR_BUILD_FOLDER
-#$root = 'C:\projects\mstc-f3l95'
+#$root = 'C:\projects\mstc-f3l95\website'
+$root =  $env:APPVEYOR_BUILD_FOLDER\website
 Write-Host $root
 
-$webconfig = get-content $root\website\Web.config | select-object -skip 1
+$webconfig = get-content $root\Web.config | select-object -skip 1
 #Write-Host $webconfig
 
 $webconfig = $webconfig -replace "<add key=`"gmailUserName`" value=`"[a-zA-Z0-9-_.:/\\@]+","<add key=`"gmailUserName`" value=`"$Env:gmailUserName"
