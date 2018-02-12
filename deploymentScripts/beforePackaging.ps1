0dc016575ab6f836186b98cc1bf7f111592df07f
Mike Hook <hook.mike@gmail.com>
2018-02-12 16:49:38 +0000

    Updated deployment script with better regex matching for replacing variables.

--- deploymentScripts/beforePackaging.ps1
+++ deploymentScripts/beforePackaging.ps1
@@ -1,6 +1,9 @@
+#Use this command to run the script
+#&("D:\dev\MSTC\deploymentScripts\beforePackaging.ps1")
+
 Write-Host 'Running before packaging script'
 
-#$root = 'D:\dev\MSTC\website'
+#$root = 'D:\Temp'
 #$root = 'C:\projects\mstc-f3l95\website'
 $root =  $env:APPVEYOR_BUILD_FOLDER + '\website'
 Write-Host $root
@@ -9,47 +12,47 @@ $configPath = $root + '\Web.config'
 $webconfig = get-content $configPath
 #Write-Host $webconfig
 
+function ReplaceAppSetting($sourceFile, $keyName, $replacementValue) {
+    $sourceFile -replace "<add key=`"$keyName`" value=`".*`"","<add key=`"$keyName`" value=`"$replacementValue`""
+}
+
 $webconfig = $webconfig -replace "connectionString=`"([^`"]+)`"","connectionString=`"$env:databaseConnectionString`""
-$webconfig = $webconfig -replace "<add key=`"umbracoDbDSN`" value=`"([^`"]+)`"","<add key=`"umbracoDbDSN`" value=`"$env:databaseDsn`""
-$webconfig = $webconfig -replace "<add key=`"triclubDSN`" value=`"([^`"]+)`"","<add key=`"triclubDSN`" value=`"$env:databaseDsn`""
-$webconfig = $webconfig -replace "<add key=`"gmailUserName`" value=`"([^`"]+)`"","<add key=`"gmailUserName`" value=`"$env:gmailUserName`""
-$webconfig = $webconfig -replace "<add key=`"gmailPassword`" value=`"([^`"]+)`"","<add key=`"gmailPassword`" value=`"$env:gmailPassword`""
+$webconfig = ReplaceAppSetting $webconfig "umbracoDbDSN" $env:databaseDsn
+$webconfig = ReplaceAppSetting $webconfig "triclubDSN" $env:databaseDsn
+$webconfig = ReplaceAppSetting $webconfig "gmailUserName" $env:gmailUserName
+$webconfig = ReplaceAppSetting $webconfig "gmailPassword" $env:gmailPassword
 if ($env:gotriEntryEmailTo)
 {
-	$webconfig = $webconfig -replace "<add key=`"gotriEntryEmailTo`" value=`"([^`"]+)`"","<add key=`"gotriEntryEmailTo`" value=`"$env:gotriEntryEmailTo`""
+	$webconfig = ReplaceAppSetting $webconfig "gotriEntryEmailTo" $env:gotriEntryEmailTo
 }
 if ($env:juniorEntryEmailTo)
 {
-	$webconfig = $webconfig -replace "<add key=`"juniorEntryEmailTo`" value=`"([^`"]+)`"","<add key=`"juniorEntryEmailTo`" value=`"$env:juniorEntryEmailTo`""
+	$webconfig = ReplaceAppSetting $webconfig "juniorEntryEmailTo" $env:juniorEntryEmailTo
 }
 if ($env:midSussexTriEntryEmailTo)
 {
-	$webconfig = $webconfig -replace "<add key=`"midSussexTriEntryEmailTo`" value=`"([^`"]+)`"","<add key=`"midSussexTriEntryEmailTo`" value=`"$env:midSussexTriEntryEmailTo`""
+	$webconfig = ReplaceAppSetting $webconfig "midSussexTriEntryEmailTo" $env:midSussexTriEntryEmailTo
 }
 if ($env:newRegistrationEmailTo)
 {
-	$webconfig = $webconfig -replace "<add key=`"newRegistrationEmailTo`" value=`"([^`"]+)`"","<add key=`"newRegistrationEmailTo`" value=`"$env:newRegistrationEmailTo`""
+	$webconfig = ReplaceAppSetting $webconfig "newRegistrationEmailTo" $env:newRegistrationEmailTo
 }
 if ($env:contactFormEmailTo)
 {
-	$webconfig = $webconfig -replace "<add key=`"contactFormEmailTo`" value=`"([^`"]+)`"","<add key=`"contactFormEmailTo`" value=`"$env:contactFormEmailTo`""
+	$webconfig = ReplaceAppSetting $webconfig "contactFormEmailTo" $env:contactFormEmailTo
 }
 
-$webconfig = $webconfig -replace "<add key=`"gocardlessEnvironment`" value=`"([^`"]+)`"","<add key=`"gocardlessEnvironment`" value=`"$env:gocardlessEnvironment`""
-$webconfig = $webconfig -replace "<add key=`"gocardlessAppId`" value=`"([^`"]+)`"","<add key=`"gocardlessAppId`" value=`"$env:gocardlessAppId`""
-$webconfig = $webconfig -replace "<add key=`"gocardlessAppSecret`" value=`"([^`"]+)`"","<add key=`"gocardlessAppSecret`" value=`"$env:gocardlessAppSecret`""
-$webconfig = $webconfig -replace "<add key=`"gocardlessToken`" value=`"([^`"]+)`"","<add key=`"gocardlessToken`" value=`"$env:gocardlessToken`""
-$webconfig = $webconfig -replace "<add key=`"gocardlessMerchantId`" value=`"([^`"]+)`"","<add key=`"gocardlessMerchantId`" value=`"$env:gocardlessMerchantId`""
-$webconfig = $webconfig -replace "<add key=`"gocardlessAccessToken`" value=`"([^`"]+)`"","<add key=`"gocardlessAccessToken`" value=`"$env:gocardlessAccessToken`""
-
+$webconfig = ReplaceAppSetting $webconfig "gocardlessEnvironment" $env:gocardlessEnvironment
+$webconfig = ReplaceAppSetting $webconfig "gocardlessMerchantId" $env:gocardlessMerchantId
+$webconfig = ReplaceAppSetting $webconfig "gocardlessAccessToken" $env:gocardlessAccessToken
 
 if ($env:renewalsEnabled)
 {
-	$webconfig = $webconfig -replace "<add key=`"renewalsEnabled`" value=`"([^`"]+)`"","<add key=`"renewalsEnabled`" value=`"$env:renewalsEnabled`""
+    $webconfig = ReplaceAppSetting $webconfig "renewalsEnabled" $env:renewalsEnabled
 }
 if ($env:openWaterEnabled)
 {
-	$webconfig = $webconfig -replace "<add key=`"openWaterEnabled`" value=`"([^`"]+)`"","<add key=`"openWaterEnabled`" value=`"$env:openWaterEnabled`""
+    $webconfig = ReplaceAppSetting $webconfig "openWaterEnabled" $env:openWaterEnabled
 }
 
 [System.IO.File]::WriteAllLines($configPath, $webconfig)
